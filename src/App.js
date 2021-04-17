4230de119bd51b92f5f389896e5a4f0704f33035
kemo12 <dodkamel919@gmail.com>
2021-04-17 13:53:36 +0300

    design stone is done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React,{useState,useEffect} from 'react'
 import { commerce } from './lib/commerce';
 import {Products,Navbar,Cart} from "./components";
+import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
 export const App = () => {
     const [products,setProducts]=useState([]);
     const [cart, setCart] = useState({});
@@ -15,16 +16,48 @@ export const App = () => {
     const fetchCart=async()=>{
         setCart(await commerce.cart.retrieve());
     }
-    const HandleAddToCart=async(productId,quanitiy)=>{
-        const item =await commerce.cart.add(productId,quanitiy);
+    const handleAddToCart = async (productId, quantity) => {
+        const item = await commerce.cart.add(productId, quantity);
+    
         setCart(item.cart);
-    }
+      };
+    
+      const handleUpdateCartQty = async (lineItemId, quantity) => {
+        const response = await commerce.cart.update(lineItemId, { quantity });
+    
+        setCart(response.cart);
+      };
+    
+      const handleRemoveFromCart = async (lineItemId) => {
+        const response = await commerce.cart.remove(lineItemId);
+    
+        setCart(response.cart);
+      };
+    
+      const handleEmptyCart = async () => {
+        const response = await commerce.cart.empty();
+    
+        setCart(response.cart);
+      };
+    
+    
     return (
-        <div>
+        <Router>
+
+        <div>   
             <Navbar totalItems={cart.total_items}/>
-            {/* <Products products={products} onAddToCart={HandleAddToCart}/> */}
-            <Cart cart={cart}/>
+            <Switch>
+            <Route exact path="/"><Products products={products} onAddToCart={handleAddToCart}/></Route>
+            <Route exact path="/cart">
+            <Cart 
+            cart={cart} 
+            onUpdateCartQty={handleUpdateCartQty} 
+            onRemoveFromCart={handleRemoveFromCart} 
+            onEmptyCart={handleEmptyCart} />
+                 </Route>
+            </Switch>
         </div>
+        </Router>
     )
 }
 export default App;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,25 +2,29 @@ import React from 'react'
 import {AppBar,Toolbar,IconButton,Badge,MenuItem,Menu, Typography} from "@material-ui/core";
 import {ShoppingCart} from "@material-ui/icons";
 import useStyles from './styles';
-import logo from "../../assetes/commerce.png"
+import logo from "../../assetes/commerce.png";
+import {Link,useLocation} from "react-router-dom";
 const Navbar = ({totalItems}) => {
     const classes = useStyles();
+    const location=useLocation();
+   
     return (
         <>
           <AppBar position="fixed" className={classes.appBar} color= "inherit">
               <Toolbar>
-                  <Typography variant="h6" className={classes.title} color="inherit">
+                  <Typography variant="h6" component={Link} to="/" className={classes.title} color="inherit">
                       <img src={logo} alt="Commerce.js" height="250px" className={classes.image}/>
                       Commerce.js
                   </Typography>
                   <div className={classes.grow}></div>
+                  {location.pathname==="/"&&(
                   <div className={classes.button}>
-                      <IconButton aria-label="Show cart items" color="inherit" >
+                      <IconButton component={Link} to="/cart" aria-label="Show cart items" color="inherit" >
                         <Badge badgeContent={totalItems} color="secondary ">
                             <ShoppingCart />
                         </Badge>
                       </IconButton>
-                  </div>
+                  </div>)}
               </Toolbar>
               </AppBar>  
         </>
